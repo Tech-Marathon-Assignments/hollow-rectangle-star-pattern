4b1267afda6bc88bef0bd243e31d1a8c67d255ef
Le Duc Duy <41518283+leduy8@users.noreply.github.com>
2021-04-05 23:16:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 """
 Bài 4: Viết chương trình vẽ hình chữ nhật rỗng bằng dấu sao ( “*” )
-(Số hàng và số cột phải là số dương lớn hơn 0)
+(Số hàng và số cột phải là số tự nhiên dương lớn hơn 0)
 Exercise 4: Write a program to print hollow rectangle star pattern.
-(Number of rows and columns must be a positive number greater than 0)
+(Number of rows and columns must be a natural positive number greater than 0)
 
 Sample input: row = 3, column = 4
 Sample output: ****
